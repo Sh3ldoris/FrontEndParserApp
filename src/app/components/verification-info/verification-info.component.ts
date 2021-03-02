85072ec85bd0edf96bbbfcaf8a2dd6c7b9233639
lanya <adam.lany@posam.sk>
2021-03-02 20:54:12 +0100

    Initial communication with validation info component

--- src/app/components/verification-info/verification-info.component.ts
+++ src/app/components/verification-info/verification-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ContainerReport} from "../../interface/container-report";
+import {ContainerService} from "../../shared-service/container.service";
 
 @Component({
   selector: 'app-verification-info',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerificationInfoComponent implements OnInit {
 
-  constructor() { }
+  report: ContainerReport;
+
+  constructor(private reportService: ContainerService) { }
 
   ngOnInit(): void {
+    this.reportService.currentReport.subscribe(rep => {
+      this.report = rep;
+    });
   }
 
 }
