c1c8421c92bfb0d597d3c458faff4445dd6e74cd
lanya <adam.lany@posam.sk>
2021-03-28 20:25:16 +0200

    Change generating pdf to client side

--- src/app/components/visualozation/visualozation.component.ts
+++ src/app/components/visualozation/visualozation.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {VisualizationsFile} from "../../interface/visualizations-file";
-import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
-import * as jsPDF from "jspdf";
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-visualozation',
@@ -33,9 +31,15 @@ export class VisualozationComponent implements OnInit {
     if (this.data.base64Pdf == null) {
       return;
     }
-    let url = URL.createObjectURL(this.base64ToBlob(this.data.base64Pdf));
-    window.open(url, '_blank');
-    URL.revokeObjectURL(url);
+
+    const option = {
+      margin:       1,
+      filename:     this.data.name.substring(0, this.data.name.length-3) + 'pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 5 },
+      jsPDF:        {unit: 'pt', format: 'a3', orientation: 'p'}
+    };
+    const worker = html2pdf().from(this.decodeBase(this.data.base64Html)).set(option).save();
   }
 
   private decodeBase(str: string) {
